54aa9ff47eeecd8e4b3a7c8a1f2066498d6a743a
Tshindimpho <mudzu13@gmail.com>
2025-01-24 11:28:18 +0200

    Add Delete endpoint for deleting contacts with custom success and error messages.

--- ContactBookAPI/Controllers/ContactsController.cs
+++ ContactBookAPI/Controllers/ContactsController.cs
@@ -57,5 +57,21 @@ namespace ContactBookAPI.Controllers
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact(Guid id)
+        {
+           var contact = await _context.Contacts.FindAsync(id);
+
+            if ( contact is not null)
+            {
+                _context.Contacts.Remove(contact);
+                await _context.SaveChangesAsync();
+            }
+
+            return StatusCode(StatusCodes.Status200OK, new { message = $"Contact with ID {id} deleted successfully." });
+
+        }
     }
 }
